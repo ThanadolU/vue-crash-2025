5cf3cd87611436fb03800c3dcda887036875497f
ThanadolU <dolthanadolu@gmail.com>
2025-08-01 12:22:17 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+  <!-- <h1 class="text-4xl font-bold text-blue-600 p-6 ">Vue Jobs</h1> -->
+  <Navbar />
+  <RouterView />
+</template>
